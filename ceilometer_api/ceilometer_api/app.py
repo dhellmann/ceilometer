f00a92b28eaa84a4514d94be771b3b877b5882ef
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-12-03 12:57:36 -0500

    Add auto-generated pecan project
    Add the files generated by pecan for the new API
    project. These will be moved into the main source
    tree during a later commit.
    
    Change-Id: Ifc549240de259f0d5472cd5146e35675141387ae
    Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

---
+++ ceilometer_api/ceilometer_api/app.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2012 New Dream Network, LLC (DreamHost)
+#
+# Author: Doug Hellmann <doug.hellmann@dreamhost.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from pecan import make_app
+from ceilometer_api import model
+from ceilometer_api import hooks
+from ceilometer.service import prepare_service
+
+
+def setup_app(config):
+
+    model.init_model()
+
+    # Initialize the cfg.CONF object
+    prepare_service([])
+
+    return make_app(
+        config.app.root,
+        static_root=config.app.static_root,
+        template_path=config.app.template_path,
+        logging=getattr(config, 'logging', {}),
+        debug=getattr(config.app, 'debug', False),
+        force_canonical=getattr(config.app, 'force_canonical', True),
+        # FIXME: Replace DBHook with a hooks.TransactionHook
+        hooks=[hooks.ConfigHook(), hooks.DBHook()],
+    )
---
+++ ceilometer_api/ceilometer_api/tests/__init__.py
@@ -0,0 +1,83 @@
+import os
+from unittest import TestCase
+
+from pecan import set_config
+from pecan.testing import load_test_app
+
+from ceilometer.openstack.common import cfg
+from ceilometer import storage
+
+__all__ = ['FunctionalTest']
+
+
+class FunctionalTest(TestCase):
+    """
+    Used for functional tests where you need to test your
+    literal application and its integration with the framework.
+    """
+
+    DBNAME = 'testdb'
+
+    def setUp(self):
+
+        cfg.CONF.database_connection = 'test://localhost/%s' % self.DBNAME
+        self.conn = storage.get_connection(cfg.CONF)
+        # Don't want to use drop_database() because we
+        # may end up running out of spidermonkey instances.
+        # http://davisp.lighthouseapp.com/projects/26898/tickets/22
+        self.conn.conn[self.DBNAME].clear()
+
+        # Determine where we are so we can set up paths in the config
+        root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '..',
+                                                '..',
+                                                )
+                                   )
+        self.config = {
+
+            'app': {
+                'root': 'ceilometer_api.controllers.root.RootController',
+                'modules': ['ceilometer_api'],
+                'static_root': '%s/public' % root_dir,
+                'template_path': '%s/ceilometer_api/templates' % root_dir,
+                'debug': True,
+                'errors': {
+                    404: '/error/404',
+                    '__force_dict__': True
+                    },
+                },
+
+            'logging': {
+                'loggers': {
+                    'root': {'level': 'INFO', 'handlers': ['console']},
+                    'ceilometer_api': {'level': 'DEBUG',
+                                       'handlers': ['console'],
+                                       },
+                    },
+                'handlers': {
+                    'console': {
+                        'level': 'DEBUG',
+                        'class': 'logging.StreamHandler',
+                        'formatter': 'simple'
+                        }
+                    },
+                'formatters': {
+                    'simple': {
+                        'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                                   '[%(threadName)s] %(message)s')
+                        }
+                    },
+                },
+            }
+
+        self.app = load_test_app(self.config)
+
+    def tearDown(self):
+        set_config({}, overwrite=True)
+
+    PATH_PREFIX = ''
+
+    def get_json(self, path, **params):
+        full_path = self.PATH_PREFIX + path
+        #print 'GET: %s %r' % (full_path, params)
+        return self.app.get(full_path, params=params).json
